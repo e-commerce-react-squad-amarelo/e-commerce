c7ad970ed6775b09f99288f8b6cef011355b6135
Falller <marlon.emanutec@gmail.com>
2022-04-12 01:37:20 -0300

    continuação da tela de cadastro

--- e-commerce/src/components/main-cadastro/index.jsx
+++ e-commerce/src/components/main-cadastro/index.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row, Stack } from "react-bootstrap";
 
 import { FormularioCadastro } from "../formulariocadastro";
+
+import { BotaoComprar } from "../botoes/botaoComprar";
 
 export function MainCadastro () {
     return (
@@ -13,7 +15,10 @@ export function MainCadastro () {
                 </Row>
 
                 <Row>
-                        aqui vai os botões
+                    <Stack direction="horizontal" gap={5} className="d-flex justify-content-center">
+                        <BotaoComprar>Salvar</BotaoComprar>
+                        <BotaoComprar>Cancelar</BotaoComprar>
+                    </Stack>  
                 </Row>
             </Container>
         </>
